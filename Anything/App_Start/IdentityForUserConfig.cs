e4cf010b6003d68316f9f59cfe3266a6878dd4e3
aobichen <aobichen@gmail.com>
2016-12-19 17:31:24 +0800

    xx

---
+++ Anything/App_Start/IdentityForUserConfig.cs
@@ -0,0 +1,207 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Helpers;
+using System.Configuration;
+using System.Net.Mail;
+using System.Net;
+using Anything.Models;
+
+namespace Anything.Models
+{
+    // Configure the application user manager used in this application. UserManager is defined in ASP.NET Identity and is used by the application.
+
+
+    public class ApplicationForUserManager : UserManager<ApplicationUser, int>
+    {
+        public ApplicationForUserManager(IUserStore<ApplicationUser, int> store)
+            : base(store)
+        {
+        }
+
+
+        public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options,
+            IOwinContext context)
+        {
+            var manager = new ApplicationForUserManager(new UserForStore(context.Get<ApplicationForUserDbContext>()));
+            // Configure validation logic for usernames
+            manager.UserValidator = new UserValidator<ApplicationUser, int>(manager)
+            {
+                AllowOnlyAlphanumericUserNames = false,
+                RequireUniqueEmail = true
+            };
+            // Configure validation logic for passwords
+            manager.PasswordValidator = new PasswordValidator
+            {
+                RequiredLength = 6,
+                RequireNonLetterOrDigit = true,
+                RequireDigit = true,
+                RequireLowercase = true,
+                RequireUppercase = true,
+            };
+            // Configure user lockout defaults
+            manager.UserLockoutEnabledByDefault = true;
+            manager.DefaultAccountLockoutTimeSpan = TimeSpan.FromMinutes(5);
+            manager.MaxFailedAccessAttemptsBeforeLockout = 5;
+            // Register two factor authentication providers. This application uses Phone and Emails as a step of receiving a code for verifying the user
+            // You can write your own provider and plug in here.
+            manager.RegisterTwoFactorProvider("PhoneCode", new PhoneNumberTokenProvider<ApplicationUser, int>
+            {
+                MessageFormat = "Your security code is: {0}"
+            });
+            manager.RegisterTwoFactorProvider("EmailCode", new EmailTokenProvider<ApplicationUser, int>
+            {
+                Subject = "SecurityCode",
+                BodyFormat = "Your security code is {0}"
+            });
+            manager.EmailService = new EmailServiceForUser();
+            manager.SmsService = new SmsService();
+            var dataProtectionProvider = options.DataProtectionProvider;
+            if (dataProtectionProvider != null)
+            {
+                manager.UserTokenProvider =
+                    new DataProtectorTokenProvider<ApplicationUser, int>(dataProtectionProvider.Create("ASP.NET Identity"));
+            }
+            return manager;
+        }
+    }
+
+    // Configure the RoleManager used in the application. RoleManager is defined in the ASP.NET Identity core assembly
+    public class ApplicationRoleForManager : RoleManager<Role, int>
+    {
+        public ApplicationRoleForManager(IRoleStore<Role, int> roleStore)
+            : base(roleStore)
+        {
+
+        }
+
+        public static ApplicationRoleManager Create(IdentityFactoryOptions<ApplicationRoleManager> options, IOwinContext context)
+        {
+            return new ApplicationRoleManager(new RoleStore<Role, int, UserRole>(context.Get<ApplicationDbContext>()));
+        }
+    }
+
+    public class EmailServiceForUser : IIdentityMessageService
+    {
+        public Task SendAsync(IdentityMessage message)
+        {
+
+            var SendMailFromWebsite = ConfigurationManager.AppSettings["SendMailFromWebsite"].ToString();
+            if (SendMailFromWebsite == "true")
+            {
+                var From = ConfigurationManager.AppSettings["From"];
+                var Password = ConfigurationManager.AppSettings["SmtpPassword"];
+                var SmtpUserName = ConfigurationManager.AppSettings["SmtpUserName"];
+                var Port = int.Parse(ConfigurationManager.AppSettings["SmtpPort"]);
+                var SmtpServer = ConfigurationManager.AppSettings["SmtpServer"];
+
+                SmtpClient smtpClient = new SmtpClient(SmtpServer, Port);
+
+
+                smtpClient.Credentials = new System.Net.NetworkCredential(From, Password);
+                //smtpClient.UseDefaultCredentials = true;              
+                smtpClient.EnableSsl = true;
+
+                MailMessage mail = new MailMessage();
+                mail.Body = message.Body;
+                mail.Subject = message.Subject;
+                mail.IsBodyHtml = true;
+                mail.SubjectEncoding = System.Text.Encoding.UTF8;
+                mail.Priority = MailPriority.Normal;
+                mail.From = new MailAddress(From, SmtpUserName);
+                mail.To.Add(new MailAddress(message.Destination));
+
+                smtpClient.Send(mail);
+                smtpClient = null;
+                mail.Dispose();
+
+
+            }
+
+            return Task.FromResult(0);
+        }
+    }
+
+    //public class SmsService : IIdentityMessageService
+    //{
+    //    public Task SendAsync(IdentityMessage message)
+    //    {
+    //        // Plug in your sms service here to send a text message.
+    //        return Task.FromResult(0);
+    //    }
+    //}
+
+    // This is useful if you do not want to tear down the database each time you run the application.
+    // public class ApplicationDbInitializer : DropCreateDatabaseAlways<ApplicationDbContext>
+    // This example shows you how to create a new database if the Model changes
+    public class ApplicationDbInitializerForUser : DropCreateDatabaseIfModelChanges<ApplicationDbContext>
+    {
+        protected override void Seed(ApplicationDbContext context)
+        {
+            InitializeIdentityForEF(context);
+            base.Seed(context);
+        }
+
+        //Create User=Admin@Admin.com with password=Admin@123456 in the Admin role        
+        public static void InitializeIdentityForEF(ApplicationDbContext db)
+        {
+            db.Configuration.LazyLoadingEnabled = true;
+            var userManager = HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            var roleManager = HttpContext.Current.GetOwinContext().Get<ApplicationRoleManager>();
+            const string name = "admin@example.com";
+            const string password = "Aa@123456";
+            const string roleName = "Admin";
+
+            //Create Role Admin if it does not exist
+            var role = roleManager.FindByName(roleName);
+            //var role = new Role(roleName);
+            if (role == null)
+            {
+
+                var roleresult = roleManager.Create(role);
+            }
+
+            var user = userManager.FindByName(name);
+            if (user == null)
+            {
+                user = new ApplicationUser { UserName = name, Email = name };
+                var result = userManager.Create(user, password);
+                result = userManager.SetLockoutEnabled(user.Id, false);
+            }
+
+            // Add user admin to Role Admin if not already added
+            var rolesForUser = userManager.GetRoles(user.Id);
+            if (!rolesForUser.Contains(role.Name))
+            {
+                var result = userManager.AddToRole(user.Id, role.Name);
+            }
+        }
+    }
+
+    public class ApplicationSignInManager : SignInManager<ApplicationUser, int>
+    {
+        public ApplicationSignInManager(ApplicationUserManager userManager, IAuthenticationManager authenticationManager)
+            : base(userManager, authenticationManager)
+        {
+        }
+
+        public override Task<ClaimsIdentity> CreateUserIdentityAsync(ApplicationUser user)
+        {
+            return user.GenerateUserIdentityAsync((ApplicationUserManager)UserManager);
+        }
+
+        public static ApplicationSignInManager Create(IdentityFactoryOptions<ApplicationSignInManager> options, IOwinContext context)
+        {
+            return new ApplicationSignInManager(context.GetUserManager<ApplicationUserManager>(), context.Authentication);
+        }
+    }
+}
