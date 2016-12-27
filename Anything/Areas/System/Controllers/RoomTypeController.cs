dbce9ac75e2614bff840dea46e818c52d2ec4bfa
aobichen <aobichen@gmail.com>
2016-12-27 17:16:27 +0800

    roomtype

---
+++ Anything/Areas/System/Controllers/RoomTypeController.cs
@@ -0,0 +1,45 @@
+﻿using Anything.Areas.System.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using Anything.Controllers;
+namespace Anything.Areas.System.Controllers
+{
+     [Authorize(Roles = "Admin,System")]
+    public class RoomTypeController : BaseController
+    {
+        // GET: System/RoomType
+         public ActionResult Index(int Page = 1)
+        {
+            var model = new RoomTypeViewModel().Query();
+            var currentPage = Page < 1 ? 1 : Page;
+            var PageSize = 10;
+
+            var result = model.ToPagedList(currentPage, PageSize);
+            ViewData.Model = result;
+            return View();
+        }
+
+         public ActionResult Edit(int? id)
+         {
+             if (id.HasValue)
+             {
+                 var model = new RoomTypeViewModel().Single(id.Value);
+                 ViewData.Model = model;
+                 return View();
+             }
+             return View();
+         }
+
+         [HttpPost]
+         public ActionResult Edit(RoomTypeViewModel model)
+         {
+
+             model.Edit();
+             return RedirectToAction("Index");
+         }
+    }
+}
---
+++ Anything/Areas/System/Models/RoomTypeViewModel.cs
@@ -0,0 +1,84 @@
+﻿using Anything.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Anything.Areas.System.Models
+{
+    public class RoomTypeViewModel
+    {
+        public int ID { get; set; }
+        public string ItemDescription { get; set; }
+        public string ItemType { get { return "Rooms"; } }
+        public string Remark { get; set; }
+        public string ItemCode { get; set; }
+        public bool Enabled { get; set; }
+        public string TypeText { get; set; }
+
+        public List<RoomTypeViewModel> Query()
+        {
+            using (var db = new MyAnythingEntities())
+            {
+                var model = db.CodeFile.Where(o => o.ItemType == "Rooms").Select(o => new RoomTypeViewModel
+                {
+                    ID = o.ID,
+                    ItemCode = o.ItemCode,
+                    ItemDescription = o.ItemDescription
+
+                }).ToList();
+                return model;
+            }
+        }
+
+        public RoomTypeViewModel Single(int id)
+        {
+            var model = new RoomTypeViewModel();
+            using (var db = new MyAnythingEntities())
+            {
+                model = db.CodeFile.Where(o => o.ItemType == "Rooms" && o.ID == id).Select(o => new RoomTypeViewModel
+                {
+                    ID = o.ID,
+                    ItemCode = o.ItemCode,
+                    ItemDescription = o.ItemDescription
+
+                }).FirstOrDefault();
+            }
+            return model;
+        }
+
+        public void Edit()
+        {
+            using (var db = new MyAnythingEntities())
+            {
+                if (ID > 0)
+                {
+                    var model = db.CodeFile.Where(o => o.ID == ID && o.ItemType == "Rooms").FirstOrDefault();
+                    if (model != null)
+                    {
+                        model.ItemDescription = ItemDescription;
+                        model.ItemType = ItemType;
+                        model.Remark = model.Remark;
+                        model.ItemCode = ItemCode;
+                        model.Enabled = model.Enabled;
+                        model.TypeText = model.TypeText;
+                        db.SaveChanges();
+                    }
+                }
+                else
+                {
+                    var model = new CodeFile();
+                    model.ItemDescription = ItemDescription;
+                    model.ItemType = "Rooms";
+                    model.Remark = string.Empty;
+                    model.ItemCode = ItemCode;
+                    model.Enabled = true;
+                    model.TypeText = "房型";
+                    db.CodeFile.Add(model);
+                    db.SaveChanges();
+                }
+            }
+
+        }
+    }
+}
